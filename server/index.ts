db8f811589fc9d26eb29259b615eea4ea69015c9
dibkb <dibas9110@gmail.com>
2024-03-04 04:00:44 +0530

    prisma mongodb setup

--- server/index.ts
+++ server/index.ts
@@ -12,6 +12,27 @@ app.use(morgan("common"));
 app.use(cors());
 app.use(express.json());
 
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  await prisma.post.create({
+    data: {
+      title: "First",
+    },
+  });
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
 app.get("/", (req: Request, res: Response) => {
   return res.json("Hello");
 });
