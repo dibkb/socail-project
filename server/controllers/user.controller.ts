3555398d9cd34515525f6d2456b3eeb8ed0ddbc6
dibkb <dibas9110@gmail.com>
2024-03-04 16:58:29 +0530

    prisma db push

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 
 export const signupUser = function async(req: Request, res: Response) {
-  res.status(200).json("success");
   try {
     const { name, email, password } = req.body;
   } catch (error: any) {
