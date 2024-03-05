e5c03abfc2bc3cc37428a61d996ff4a60547bf8e
dibkb <dibas9110@gmail.com>
2024-03-05 19:51:10 +0530

    background theme

--- client/app/auth/signup/page.tsx
+++ client/app/auth/signup/page.tsx
@@ -1,7 +1,40 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 import React from "react";
 
 const Signup = () => {
-  return <div className="border"></div>;
+  return (
+    <Card className="min-w-[400px]">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default Signup;
