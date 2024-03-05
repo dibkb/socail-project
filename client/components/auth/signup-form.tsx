bcbe59cb2be175338fe036e617f56a39e8721120
dibkb <dibas9110@gmail.com>
2024-03-05 23:48:39 +0530

    Singup

--- client/components/auth/signup-form.tsx
+++ client/components/auth/signup-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -11,28 +12,71 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { useForm } from "react-hook-form";
+import { signinSchema } from "@/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
 const SignupForm = () => {
+  const form = useForm<z.infer<typeof signinSchema>>({
+    resolver: zodResolver(signinSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
   return (
-    <div>
+    <div className="">
       <Card className="min-w-[400px] border-none drop-shadow-2xl">
         <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription>
-            Deploy your new project in one-click.
-          </CardDescription>
+          <CardTitle className="text-center">
+            Log in with your Github account
+          </CardTitle>
         </CardHeader>
         <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
           <form>
             <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" placeholder="Name of your project" />
+              <div className="flex flex-col bg-stone-900 rounded-xl">
+                <Input
+                  id="name"
+                  placeholder="Username"
+                  className="focus-visible:ring-stone-700 py-6 rounded-xl
+                  "
+                />
               </div>
             </div>
           </form>
         </CardContent>
         <CardFooter className="flex justify-between">
-          <Button variant="outline">Cancel</Button>
           <Button>Deploy</Button>
         </CardFooter>
       </Card>
---
+++ client/schemas/index.ts
@@ -0,0 +1,15 @@
+import * as z from "zod";
+export const signinSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(3, {
+    message: "Minimum 3 characters required",
+  }),
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+  username: z.string().min(1, {
+    message: "Username is required",
+  }),
+});
