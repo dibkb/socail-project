47a2bd5536a9f25feaf9defb4e8ff734a8b2ffd1
dibkb <dibas9110@gmail.com>
2024-03-12 01:43:35 +0530

    dropdown menu

--- client/components/nav/menu-icon.tsx
+++ client/components/nav/menu-icon.tsx
@@ -1,3 +1,4 @@
+// "use client";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,19 +8,28 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { TbMenu } from "react-icons/tb";
 import menuitems from "../../utils/nav-dropdown-menu";
+import { cn } from "@/lib/utils";
 export function DropdownMenuDemo() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild className="cursor-pointer">
         <TbMenu className="h-8 w-8 text-stone-700 hover:text-white" />
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56 border-none bg-stone-800">
-        <DropdownMenuGroup className="flex flex-col gap-y-3">
-          {menuitems.map((item) => (
-            <DropdownMenuItem key={item} className="capitalize">
-              {item}
-            </DropdownMenuItem>
-          ))}
+      <DropdownMenuContent className="w-44 border-none bg-stone-800 rounded-xl">
+        <DropdownMenuGroup className="flex flex-col">
+          {menuitems.map((item, id) => {
+            return (
+              <DropdownMenuItem
+                key={item}
+                className={cn(
+                  "rounded-none capitalize cursor-pointer py-3 border-stone-700",
+                  `${id === menuitems.length - 1 ? `border-none` : `border-b`}`
+                )}
+              >
+                {item}
+              </DropdownMenuItem>
+            );
+          })}
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
