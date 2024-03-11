c7bd9edb8309587409959e702e11dc7d78b07091
dibkb <dibas9110@gmail.com>
2024-03-12 00:37:58 +0530

    more abstraction

--- client/components/nav/navbar.tsx
+++ client/components/nav/navbar.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import { FaThreads } from "react-icons/fa6";
 import { GoHome, GoHomeFill } from "react-icons/go";
 import Navicons from "./nav-icons";
+const classNames: Record<"active" | "inactive", HTMLElement["className"]> = {
+  active: "h-7 w-7",
+  inactive: "h-7 w-7 text-stone-600",
+};
 const Navbar = () => {
   return (
     <nav className="flex justify-between min-h-14 py-1">
@@ -13,9 +17,14 @@ const Navbar = () => {
       <div className="flex gap-x-1">
         <Navicons
           src={"/"}
-          active={<GoHomeFill className="h-7 w-7" />}
-          inactive={<GoHome className="h-7 w-7" />}
-        ></Navicons>
+          active={<GoHomeFill className={classNames.active} />}
+          inactive={<GoHome className={classNames.inactive} />}
+        />
+        <Navicons
+          src={"/search"}
+          active={<GoHomeFill className={classNames.active} />}
+          inactive={<GoHome className={classNames.inactive} />}
+        />
       </div>
       {/* rights */}
       <div></div>
