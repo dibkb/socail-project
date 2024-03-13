6a1b366596bb56dab4d12a532b070e57781a48ac
dibkb <dibas9110@gmail.com>
2024-03-13 20:08:46 +0530

    threads form

--- client/components/home/thread-form.tsx
+++ client/components/home/thread-form.tsx
@@ -4,7 +4,7 @@ import { Button } from "../ui/button";
 
 const Threadform = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-y-6">
       <div className="flex items-center w-full justify-between gap-4 cursor-pointer">
         <AvatarForm />
         <span className="text-sm text-stone-500 select-none mr-auto">
@@ -14,6 +14,7 @@ const Threadform = () => {
           Post
         </Button>
       </div>
+      <hr className="border-stone-800 border-t border-[1/2px]" />
     </div>
   );
 };
