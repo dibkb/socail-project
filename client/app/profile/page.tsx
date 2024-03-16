a21836072723097659f94734ac42924211457fb9
dibkb <dibas9110@gmail.com>
2024-03-16 14:51:41 +0530

    start thread modal

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -1,10 +1,19 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import React from "react";
-
+import React, { useState } from "react";
+import ThreadformPortal from "@/modals/thread-modal";
 const Profilepage = () => {
-  const noThread = <Button variant={"outline"}>Start your first thread</Button>;
+  const [openThreadModal, setOpenThreadModal] = useState(false);
+  const noThread = (
+    <Button onClick={() => setOpenThreadModal(true)} variant={"outline"}>
+      Start your first thread
+    </Button>
+  );
   return (
-    <div className="grow flex items-center justify-center">{noThread}</div>
+    <div className="grow flex items-center justify-center">
+      {!openThreadModal && noThread}
+      {openThreadModal && <ThreadformPortal setOpen={setOpenThreadModal} />}
+    </div>
   );
 };
 
