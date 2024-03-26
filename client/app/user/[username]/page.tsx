9bc38f770f0c44fddc0cefbad5f79de7cb8e5626
dibkb <dibas9110@gmail.com>
2024-03-27 02:30:35 +0530

    user page

--- client/app/user/[username]/page.tsx
+++ client/app/user/[username]/page.tsx
@@ -1,12 +1,31 @@
+"use client";
 import { Globallayout, Profilelayout } from "@/components/layouts/main";
+import NameUsername, {
+  Bio,
+} from "@/components/profile/profile-page/name-username";
 import React from "react";
-
-const Username = () => {
+import AvatarForm from "@/components/home/avatar";
+import { useUserStore } from "@/src/providers/user-store-provider";
+export default function Username() {
+  const { user } = useUserStore((state) => state);
   return (
     <Globallayout>
-      <Profilelayout>fsdf</Profilelayout>
+      <Profilelayout>
+        {" "}
+        <div className="flex items-center justify-between">
+          <NameUsername name={"The Stiletto Stoner"} username={"imshotta"} />
+          <aside>
+            <AvatarForm className="w-20 h-20" />
+          </aside>
+        </div>
+        <main className="mt-4">
+          <Bio
+            bio={
+              "I DROP GEMS 💎GOD 1st GOD Willing اللہ for bookings contact getatshotta@gmail.com “Dats Right” Out Now ‼️‼️"
+            }
+          />
+        </main>
+      </Profilelayout>
     </Globallayout>
   );
-};
-
-export default Username;
+}
