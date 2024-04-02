ab8df34964fe0eba51610113683824ce738984ef
dibkb <dibas9110@gmail.com>
2024-04-02 21:20:14 +0530

    remove useliess imorts

--- client/app/user/[username]/page.tsx
+++ client/app/user/[username]/page.tsx
@@ -1,38 +1,23 @@
 // "use client";
 import { getUserPostsByUsername } from "@/actions/getUserPosts";
-import { Post, Threads } from "@/types";
-import { usePathname } from "next/navigation";
 import React, { Suspense } from "react";
-import Spinner from "@/components/svg/spinner";
 import Posts from "@/components/posts/post";
-import Loading from "@/components/guides/loading";
 import { PostSkeleton } from "@/components/guides/skeleton-loader";
-import {
-  getCleanedusername,
-  getCleanedusernameServer,
-} from "@/utils/getCleanedusername";
+import { getCleanedusernameServer } from "@/utils/getCleanedusername";
 import { getAllusernames } from "@/actions/getuser";
-import { smallProfileFetcher } from "@/actions/getComment";
 export async function generateStaticParams() {
   const usernames = await getAllusernames();
   return usernames.data.map((u: any) => ({
     username: `@${u.username}`,
   }));
 }
-interface posts {
-  posts: Post[];
-  threads: Threads[];
-}
 const Userprofilepage = async ({
   params,
 }: {
   params: { username: string };
 }) => {
-  const cleanedUsername = (await getCleanedusernameServer(
-    params.username
-  )) as string;
-  const { data } = await getUserPostsByUsername(cleanedUsername);
-  console.log(data?.posts?.length && data?.threads?.length);
+  const cleanedUsername = getCleanedusernameServer(params.username) as string;
+  const { data, error } = await getUserPostsByUsername(cleanedUsername);
   return (
     <Suspense fallback={<PostSkeleton />}>
       {data?.posts?.length === 0 && data?.threads?.length === 0 ? (
