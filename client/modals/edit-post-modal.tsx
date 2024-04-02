014517d0dc3f0cf4feaeda0a94b89276fb062b14
dibkb <dibas9110@gmail.com>
2024-04-02 08:12:22 +0530

    edit post modal

--- client/modals/edit-post-modal.tsx
+++ client/modals/edit-post-modal.tsx
@@ -2,19 +2,34 @@ import React, { Dispatch, SetStateAction, useState } from "react";
 import Modallayout from "./modal-layout";
 import styles from "@/styles/edit-post";
 import { Post } from "@/types";
-import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { RxCross1 } from "react-icons/rx";
+import { TbPhoto } from "react-icons/tb";
+import { resizeFile } from "@/utils/compress-image";
 interface Editpostmodal {
   setOpen: Dispatch<SetStateAction<boolean>>;
   post: Post;
 }
 const Editpostmodal = ({ setOpen, post }: Editpostmodal) => {
   const [body, setBody] = useState<string>(post.body);
+  const [image, setImage] = useState<string>(post?.image || "");
   const changeTextAreaHandler = (
     event: React.ChangeEvent<HTMLTextAreaElement>
   ) => {
     event.target.style.height = "auto";
     event.target.style.height = event.target.scrollHeight + "px";
     setBody(event.target.value);
+  };
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files) {
+      const file = files[0];
+      if (file && file.type.startsWith("image/")) {
+        const image = (await resizeFile(file)) as string;
+        setImage(image);
+      } else {
+      }
+    }
   };
   return (
     <Modallayout setOpen={setOpen}>
@@ -25,7 +40,7 @@ const Editpostmodal = ({ setOpen, post }: Editpostmodal) => {
           style={styles.container}
           //   onSubmit={createPostHandler}
         >
-          <main className="flex gap-x-4" style={styles.main}>
+          <main className="flex flex-col gap-x-4" style={styles.main}>
             <textarea
               value={body}
               placeholder={"Edit post modal here ..."}
@@ -33,6 +48,33 @@ const Editpostmodal = ({ setOpen, post }: Editpostmodal) => {
               style={styles.textarea}
               onChange={changeTextAreaHandler}
             />
+            {image && (
+              <div className="relative mb-2">
+                <Image
+                  src={image}
+                  alt={"Image assocaited with " + post.body}
+                  width={300}
+                  height={300}
+                  className="rounded-lg"
+                ></Image>
+                <RxCross1
+                  onClick={() => setImage("")}
+                  className="p-2 rounded-full absolute top-0 right-0 bg-stone-700 w-9 h-9 hover:bg-stone-600 cursor-pointer"
+                />
+              </div>
+            )}
+            <input
+              type="file"
+              id={`imageinput`}
+              hidden
+              onChange={(e) => handleFileChange(e)}
+            />
+            <label htmlFor={`imageinput`}>
+              <TbPhoto
+                size={18}
+                className="cursor-pointer text-stone-500 hover:text-white"
+              />
+            </label>
           </main>
         </form>
       </div>
@@ -41,3 +83,6 @@ const Editpostmodal = ({ setOpen, post }: Editpostmodal) => {
 };
 
 export default Editpostmodal;
+function setImgUrl(arg0: (prev: any) => any[]) {
+  throw new Error("Function not implemented.");
+}
