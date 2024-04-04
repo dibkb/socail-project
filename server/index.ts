d2a88b1bce47b3304ef7efc4a1d9f40fdd5d7243
dibkb <dibas9110@gmail.com>
2024-04-04 14:03:51 +0530

    new process.env

--- server/index.ts
+++ server/index.ts
@@ -19,12 +19,20 @@ const app = express();
 dotenv.config();
 
 // middleware
+app.set("trust proxy", 1);
 app.use(helmet());
 app.use(morgan("common"));
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
+const allowedOrigins = ["http://localhost:3000", "https://shreads.vercel.app"];
+
+const corsOptions: cors.CorsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // Allow credentials (cookies)
 };
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "3mb" }));
--- server/utils/helpers.ts
+++ server/utils/helpers.ts
@@ -13,7 +13,7 @@ export const generateAndSetCookie = ({
     httpOnly: true,
     maxAge: 15 * 24 * 60 * 60 * 1000,
     sameSite: "none",
-    secure: false,
+    secure: true,
   });
   return token;
 };
