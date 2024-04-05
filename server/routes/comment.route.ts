e95e89949da666109c26806a341fc0e039970911
dibkb <dibas9110@gmail.com>
2024-04-06 00:24:25 +0530

    minor errpr

--- server/routes/comment.route.ts
+++ server/routes/comment.route.ts
@@ -8,8 +8,8 @@ import {
 } from "../controllers/comment.controller";
 
 const router = express.Router();
-router.post("/:postid", verifyRoute, createComment);
-router.get("/:postid", verifyRoute, getComments);
-router.get("/user/all", verifyRoute, getCommentsUser);
-router.get("/user/:userid/all", verifyRoute, getCommentsByUserId);
+router.post("/:postid", verifyRoute as any, createComment as any);
+router.get("/:postid", verifyRoute as any, getComments as any);
+router.get("/user/all", verifyRoute as any, getCommentsUser as any);
+router.get("/user/:userid/all", verifyRoute as any, getCommentsByUserId as any);
 export default router;
--- server/routes/post.route.ts
+++ server/routes/post.route.ts
@@ -17,19 +17,19 @@ import { getThread } from "../controllers/thread.controller";
 
 const router = express.Router();
 // ------------GET--------------
-router.get("/threads/:threadId", verifyRoute, getThread);
-router.get("/all", verifyRoute, getEveryPost);
-router.get("/:postid", verifyRoute, getPost);
-router.get("/all/:userid", verifyRoute, getAllPosts);
-router.get("/all/username/:username", getAllPostsByUsername);
+router.get("/threads/:threadId", verifyRoute as any, getThread as any);
+router.get("/all", verifyRoute as any, getEveryPost as any);
+router.get("/:postid", verifyRoute as any, getPost as any);
+router.get("/all/:userid", verifyRoute as any, getAllPosts as any);
+router.get("/all/username/:username", getAllPostsByUsername as any);
 // ------------POST--------------
-router.post("/like/:postid", verifyRoute, likePost);
-router.post("/unlike/:postid", verifyRoute, unlikePost);
-router.post("/create", verifyRoute, createPost);
-router.post("/threads/create", verifyRoute, createThreads);
+router.post("/like/:postid", verifyRoute as any, likePost as any);
+router.post("/unlike/:postid", verifyRoute as any, unlikePost as any);
+router.post("/create", verifyRoute as any, createPost as any);
+router.post("/threads/create", verifyRoute as any, createThreads as any);
 // -------------PUT----------------
-router.put("/:postid", verifyRoute, updatePost);
+router.put("/:postid", verifyRoute as any, updatePost as any);
 // --------------DELTETE-----------
-router.delete("/:postid", verifyRoute, deletePost);
+router.delete("/:postid", verifyRoute as any, deletePost as any);
 
 export default router;
