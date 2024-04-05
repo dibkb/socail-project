19f556f330dd2f475878296b468f4dfdfbd1c716
dibkb <dibas9110@gmail.com>
2024-04-05 17:51:16 +0530

    commnets responsive and linking

--- client/components/posts/post-comment.tsx
+++ client/components/posts/post-comment.tsx
@@ -6,6 +6,8 @@ import { calulateTime } from "@/utils/calulate-time-passed";
 import useSWR, { SWRResponse } from "swr";
 import { smallProfileFetcher } from "@/actions/getComment";
 import { User } from "@/src/stores/user-store";
+import { useIsBelowWidth } from "@/hooks/isBelowWidth";
+import Link from "next/link";
 interface Postcomment {
   com: Comment;
 }
@@ -18,19 +20,25 @@ const Postcomment = React.memo(({ com }: Postcomment) => {
     com.userId,
     smallProfileFetcher
   );
+  const { isBelowWidth } = useIsBelowWidth(399);
   return (
-    <main className="rounded-full flex justify-between">
+    <main className="flex justify-between items-start">
       <div className="flex items-center grow gap-2">
-        <span className="flex items-center gap-2">
+        <Link
+          className="flex items-center gap-2 hover:underline"
+          href={`user/@${data?.username}`}
+        >
           <Avatar
             variant={"others"}
             imgurl={data?.profilePic}
             name={data?.username}
             className="h-8 w-8"
           />
-          <p className="text-sm">{data?.username}</p>
-        </span>
-        <p className="text-sm font-medium text-stone-400 grow">{com.body}</p>
+          {isBelowWidth ? "" : <p className="text-sm ">{data?.username}</p>}
+        </Link>
+        <p className="text-sm font-medium text-stone-400 grow text-clip line-clamp-1">
+          {com.body}
+        </p>
         <span className="flex items-center gap-1 text-stone-600 text-xs font-medium">
           <p>{calulateTime(com.createdAt).quantity}</p>
           <p>{calulateTime(com.createdAt).unit}</p>
